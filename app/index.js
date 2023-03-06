c1a24169594b4f788510ddf823309aeb3fb0d7fe
AKintunde Owojori <owojoriakintunde@gmail.com>
2023-03-06 21:52:08 +0530

    Working

--- app/index.js
+++ app/index.js
@@ -20,6 +20,7 @@ import {categories, collections, user} from "../data";
 import Font from "../constants/Font";
 import Layout from "../constants/Layout";
 import {BlurView} from "expo-blur";
+import {Link} from "expo-router";
 
 const SIZE = Spacing * 6;
 const NFT_HEIGHT = Spacing * 45;
@@ -47,18 +48,240 @@ const index = () => {
   }, []);
 
   return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <View>
+    <ScrollView>
+      <SafeAreaView
+        style={{
+          marginTop: 25,
+        }}
+      >
+        <View
+          style={{
+            paddingHorizontal: Spacing * 2,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <TouchableOpacity>
-            <Text>Hello world</Text>
+            <MaterialCommunityIcons
+              name="dots-grid"
+              color={Colors.text}
+              size={Spacing * 4}
+            />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Button title="Show Toast" onPress={showToast} />
+          <Text
+            style={{
+              color: Colors.text,
+              fontSize: Spacing * 2,
+              fontFamily: Font.gilroyBold,
+            }}
+          >
+            {user.name}
+          </Text>
+          <TouchableOpacity
+            style={{
+              height: SIZE,
+              width: SIZE,
+              overflow: "hidden",
+              borderRadius: SIZE / 2,
+            }}
+          >
+            <Image
+              source={user.image}
+              style={{width: "100%", height: "100%"}}
+            />
           </TouchableOpacity>
         </View>
+
+        <View
+          style={{
+            backgroundColor: Colors.lightBackground,
+            marginVertical: Spacing * 3,
+          }}
+        >
+          <ScrollView
+            style={{
+              paddingVertical: Spacing * 2,
+            }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {categories.map((category) => (
+              <TouchableOpacity
+                onPress={() => setActiveCategory(category)}
+                style={{
+                  paddingHorizontal: Spacing * 3,
+                }}
+                key={category.id}
+              >
+                <Text
+                  style={[
+                    {
+                      fontSize: Spacing * 2,
+                      color: Colors.lightText,
+                      fontFamily: Font.gilroyMedium,
+                    },
+                    activeCategory?.id === category.id && {
+                      fontFamily: Font.gilroyBold,
+                      color: Colors.darkText,
+                    },
+                  ]}
+                >
+                  {" "}
+                  {category.name}{" "}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: Spacing * 2,
+            paddingVertical: Spacing * 4,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: Spacing * 2,
+              color: Colors.text,
+              fontFamily: Font.gilroyMedium,
+            }}
+          >
+            {" "}
+            {activeCategory?.name} collections
+          </Text>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: Colors.lightText,
+                fontSize: Spacing * 1.6,
+              }}
+            >
+              The week
+            </Text>
+            <Ionicons
+              name="chevron-down"
+              size={Spacing * 3}
+              color={Colors.lightText}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          {collectionList.map((collection) => (
+            <View
+              key={collection.id}
+              style={{
+                position: "relative",
+                width: "100%",
+                alignItems: "center",
+              }}
+            >
+              {collection.nfts.map((nft, index) => (
+                <ImageBackground
+                  style={{
+                    height: NFT_HEIGHT,
+                    width: NFT_WIDTH,
+                    position: "absolute",
+                    zIndex: Spacing - index,
+                    transform: [
+                      {
+                        translateY: index * 10,
+                      },
+                      {
+                        scaleX: 1 - index / 10,
+                      },
+                    ],
+                    borderRadius: Spacing * 3,
+                    overflow: "hidden",
+                    justifyContent: "flex-end",
+                  }}
+                  key={nft.id}
+                  source={nft.image}
+                >
+                  {index === 0 && (
+                    <BlurView
+                      tint="dark"
+                      intensity={30}
+                      style={{
+                        bottom: Spacing * 3,
+                        marginHorizontal: Spacing * 2,
+                        borderRadius: Spacing * 3,
+                        overflow: "hidden",
+                        padding: Spacing / 2,
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <View
+                        style={{
+                          paddingHorizontal: Spacing,
+                          paddingVertical: Spacing,
+                        }}
+                      >
+                        <Text
+                          style={{
+                            color: Colors.text,
+                            fontFamily: Font.gilroyBold,
+                            fontSize: Spacing * 3,
+                          }}
+                        >
+                          {collection.name}
+                        </Text>
+                        <Text
+                          style={{
+                            color: Colors.text,
+                            fontFamily: Font.gilroyLight,
+                            fontSize: Spacing * 1.7,
+                          }}
+                        >
+                          {collection.handle}
+                        </Text>
+                      </View>
+                      <Link
+                        href={{
+                          pathname: "/DetailScreen",
+                          // /* 1. Navigate to the details route with query params */
+                          params: {collection: collection},
+                        }}
+                        // onPress={() =>
+                        //   navigate("DetailScreen", {collection: collection})
+                        // }
+                        style={{
+                          padding: Spacing * 2.5,
+                          backgroundColor: Colors.secondary,
+                          borderRadius: Spacing * 3,
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontFamily: Font.gilroyBold,
+                            color: Colors.text,
+                            fontSize: Spacing * 1.7,
+                          }}
+                        >
+                          View Collection
+                        </Text>
+                      </Link>
+                    </BlurView>
+                  )}
+                </ImageBackground>
+              ))}
+            </View>
+          ))}
+        </View>
       </SafeAreaView>
-    </>
+    </ScrollView>
   );
 };
 
